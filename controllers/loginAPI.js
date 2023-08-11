bf901a02b96da8f885c2acf62994e2bbcdeaa37e
conglapgit45 <conglap45@gmail.com>
2023-08-11 13:58:52 +0700

    6th

--- controllers/loginAPI.js
+++ controllers/loginAPI.js
@@ -19,7 +19,7 @@ exports.LoginAPI = async (req, res, next) => {
                 length: 49,
                 charset: ['alphabetic', 'numeric']
             })
-            res.cookie('authToken', authToken, { maxAge: 1000*60*60*4, httpOnly: true, secure: true, signed: true })
+            res.cookie('authToken', authToken, { sameSite: 'none', maxAge: 1000*60*60*4, httpOnly: true, secure: true, signed: true })
             res.cookie('authToken', authToken)
             req.session.authToken = authToken
             console.log('login: ' + req.session.authToken)
