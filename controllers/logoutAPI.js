793d9ed658d19a637fc2a79e424e0edb96a950c5
conglapgit45 <conglap45@gmail.com>
2023-09-04 09:56:37 +0700

    11th

--- controllers/logoutAPI.js
+++ controllers/logoutAPI.js
@@ -6,7 +6,7 @@ exports.LogoutAPI = async (req, res, next) => {
     try {
         // const authToken = await req.session.authToken
         // console.log('out: ' + authToken)
-        console.log('out cookie: ' + req.signedCookies.authToken)
+        console.log('out cookie: ' + await req.signedCookies.authToken)
         res.clearCookie('accessAuth').send()
         res.clearCookie('authToken').send()
         // res.clearCookie('session').send()
