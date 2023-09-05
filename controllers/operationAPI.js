bd9afff5c96e87bb10cc9291c4751f5de0629395
conglapgit45 <conglap45@gmail.com>
2023-09-05 10:45:45 +0700

    31th

--- controllers/operationAPI.js
+++ controllers/operationAPI.js
@@ -6,11 +6,13 @@ require('dotenv').config()
 
 exports.OperationAPI = async (req, res, next) => {
     try {
-        console.log("CHeck")
+        console.log("CHeck 01")
         var itemsData = await itemsSchema.find({})
         itemsData = [...new Map(itemsData.map(item => [item['item_code'], item])).values()]
         itemsData = itemsData.sort((a, b) => parseFloat(a.item_code) - parseFloat(b.item_code))
+        console.log("CHeck 02")
         res.status(201).json({data: itemsData, message: 'List of items'})
+        console.log("CHeck 03")
     }
     catch (error) {
         console.log(error)
