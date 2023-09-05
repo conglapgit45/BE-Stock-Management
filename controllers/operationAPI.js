1daa081d51b6da7cc62ecaba38e58ee5ea57d0ea
conglapgit45 <conglap45@gmail.com>
2023-09-05 11:06:30 +0700

    33th

--- controllers/operationAPI.js
+++ controllers/operationAPI.js
@@ -11,7 +11,7 @@ exports.OperationAPI = async (req, res, next) => {
         itemsData = [...new Map(itemsData.map(item => [item['item_code'], item])).values()]
         itemsData = itemsData.sort((a, b) => parseFloat(a.item_code) - parseFloat(b.item_code))
         console.log("CHeck 02")
-        res.status(201).json({data: itemsData, message: 'List of items'})
+        return res.status(201).json({data: itemsData, message: 'List of items'})
         console.log("CHeck 03")
     }
     catch (error) {
