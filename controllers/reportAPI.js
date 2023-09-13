76a60bc4d015c5d9787a1575f4396f02bb60278c
conglapgit45 <conglap45@gmail.com>
2023-09-13 15:13:18 +0700

    55th

--- controllers/reportAPI.js
+++ controllers/reportAPI.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser')
 exports.ReportAPI = async (req, res, next) => {
     try {
         console.log('Report cookie: ' + await req.signedCookies.authToken)
+        
     }
     catch (error) {
         console.error(error)
