66a96c29e572a0d2dd6fd24de6c53121de4bbb34
conglapgit45 <conglap45@gmail.com>
2023-09-14 14:42:48 +0700

    73th

--- controllers/reportAPI.js
+++ controllers/reportAPI.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken')
 exports.ReportAPI = async (req, res, next) => {
     try {
         const accessToken = await req.signedCookies.accessToken
-        console.log('Report cookie: ' + jwt.verify(accessToken, process.env.JWT_SECRET).accessToken)
+        console.log('Report cookie: ' + jwt.verify(accessToken, process.env.JWT_SECRET))
     }
     catch (error) {
         console.error(error)
