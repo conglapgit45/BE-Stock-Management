8f95c549f06cf412fdb31ac2fe9c561075e17342
conglapgit45 <conglap45@gmail.com>
2023-09-14 14:19:53 +0700

    64th

--- middleWares/authChecker.js
+++ middleWares/authChecker.js
@@ -15,6 +15,7 @@ exports.AuthChecking = async (req, res, next) => {
         if (jwt.verify(accessToken, process.env.JWT_SECRET)) {
             isLoggedIn = true
             console.log(isLoggedIn)
+            console.log(jwt.verify(accessToken, process.env.JWT_SECRET))
             // next()
         }
         if (isLoggedIn == true && jwt.verify(accessToken, process.env.JWT_SECRET).role == 'ADMIN') {
